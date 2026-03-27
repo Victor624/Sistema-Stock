0afd96dfa7951b0909a8410683f700fb7adf19c6
Victor624 <victorvargas023@gmail.com>
2026-03-26 21:26:00 -0300

    "cambios"

--- db.py
+++ db.py
@@ -1,25 +1,25 @@
+import os
 from typing import Annotated
-
 
 from fastapi import Depends
 from sqlmodel import Session, SQLModel, create_engine
 
+SQLITE_DB_NAME = os.getenv("SQLITE_DB_NAME", "db.sqlite3")
+DEBUG = os.getenv("DEBUG", "false").lower() == "true"
 
-sqlite_name = "db.sqlite3"
-sqlite_url = f"sqlite:///{sqlite_name}"
+DATABASE_URL = f"sqlite:///{SQLITE_DB_NAME}"
 
-
-connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
-
+engine = create_engine(
+    DATABASE_URL,
+    echo=DEBUG,
+    connect_args={"check_same_thread": False},
+)
 
 def create_all_tables():
     SQLModel.metadata.create_all(engine)
 
-
 def get_session():
     with Session(engine) as session:
         yield session
-
 
 SessionDep = Annotated[Session, Depends(get_session)]
--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from db import  create_all_tables
+
+from db import create_all_tables
 from routers import stock
 from routers.auth import router as auth_router
 
-app = FastAPI()
+app = FastAPI(
+    title="Aplicacion-Stock",
+    version="1.0.0"
+)
+
 app.include_router(stock.router)
 app.include_router(auth_router)
 
-
-app.title = "Aplicacion-Stock"
-app.version = "1.0.0"
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 @app.on_event("startup")
 def on_startup():
     create_all_tables()
@@ -26,9 +31,4 @@ def on_startup():
 @app.get("/")
 def root():
     return {"message": "API funcionando"}
-
-
-
-# usuarios-----------------------------------------------
-
 
